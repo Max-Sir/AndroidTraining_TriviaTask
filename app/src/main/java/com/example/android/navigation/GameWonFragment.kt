3c18cd35a6a1e5bcaf3806256e604c6b0dd61c52
Max-Sir <maksimsyromolotov@coherentsolutions.com>
2021-08-16 12:30:20 +0300

    bugs fixed and added vibration

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,10 +16,20 @@
 
 package com.example.android.navigation
 
+import android.app.Activity
+import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.util.Log
 import android.view.*
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ShareCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -31,12 +41,25 @@ class GameWonFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val vibro=activity?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if(vibro.hasVibrator()){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                vibro.vibrate(VibrationEffect.createOneShot(1000,VibrationEffect.DEFAULT_AMPLITUDE))
+            }
+            else{
+                vibro.vibrate(1000)
+            }
+        }
+        else{
+            Log.i("Vibrator_Service","Cannot perform such effect")
+        }
         // Inflate the layout for this fragment
         args = GameWonFragmentArgs.fromBundle(arguments!!)
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_game_won, container, false
         )
         binding.nextMatchButton.setOnClickListener { findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment) }
+
 
         setHasOptionsMenu(true)
         Toast.makeText(
--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -17,10 +17,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
@@ -28,18 +30,60 @@ import com.example.android.navigation.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("or","created")
+
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        val navController=this.findNavController(R.id.myNavHostFragment)
-        drawerLayout=binding.drawerLayout
-        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
-        NavigationUI.setupWithNavController(binding.navView,navController)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        drawerLayout = binding.drawerLayout
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("or","Destroyed")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("or","Paused")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("or","Resumed")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("or","Started")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("or","Stopped")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("or","Restarted")
+    }
+
+
+
     override fun onSupportNavigateUp(): Boolean {
-        val navController=this.findNavController(R.id.myNavHostFragment)
-        return NavigationUI.navigateUp(navController,drawerLayout)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 
 }
